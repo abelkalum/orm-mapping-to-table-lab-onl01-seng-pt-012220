a0274f66cff32044592c7ac0a1f4b26da42d96bc
Abel Kalum <chumbabel@gmail.com>
2020-03-31 18:09:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -22,7 +22,13 @@ class Student
   
   def self.drop_table
     sql =  <<-SQL
-      
+      DROP TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        grade TEXT
+        )
+        SQL
+    DB[:conn].execute(sql) 
   end
   
 end
