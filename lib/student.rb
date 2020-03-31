2533cb08da7815a6cb21da7ec13944b7af190131
Abel Kalum <chumbabel@gmail.com>
2020-03-31 18:02:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,6 +10,14 @@ class Student
   end
   
   def create_table
-    
+    sql =  <<-SQL 
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        album TEXT
+        )
+        SQL
+    DB[:conn].execute(sql) 
+  end
   
 end
